40651e0535494cede261b2dd8183225d3d66a2be
Stelios Tsagkarakis <tsagarakis.stelios@gmail.com>
2022-11-17 10:58:03 +0200

    I got the perfect score in pylint

--- package_statistics.py
+++ package_statistics.py
@@ -11,7 +11,7 @@
         Debian mirror. The program should parse the file and
         output the statistics of the top 10 packages that have
         the most files associated with them.
-       
+
     Exit codes:
          0: Execution completed succesfully
         -1: Could not download file
@@ -27,9 +27,10 @@ import argparse
 import os
 import gzip
 import re
+import sys
 
 
-def parse_arguments(filename: str, archs: List[str]) -> argparse.Namespace:
+def parse_arguments(fname: str, archs: List[str]) -> argparse.Namespace:
     """Parse the command line arguments. Exits if required arguments conditions are not met.
 
     Args:
@@ -41,7 +42,7 @@ def parse_arguments(filename: str, archs: List[str]) -> argparse.Namespace:
     """
 
     argparser = argparse.ArgumentParser(
-        usage="python3 " + filename + " <arch>",
+        usage="python3 " + fname + " <arch>",
         description="python command line tool that takes the architecture\
                     (amd64, arm64, mips etc.) as an argument and downloads\
                     the compressed Contents file associated with it from a\
@@ -50,22 +51,22 @@ def parse_arguments(filename: str, archs: List[str]) -> argparse.Namespace:
     )
 
     # pop the "optional" group to add required group only
-    optional = argparser._action_groups.pop()
+    # optional = argparser._action_groups.pop()
     # argparser._action_groups.append(optional)
-    required = argparser.add_argument_group("required arguments")
-    optional = argparser.add_argument_group("optional arguments")
+    # required = argparser.add_argument_group("required arguments")
+    # optional = argparser.add_argument_group("optional arguments")
 
     # join the architectures in a comma separated string
     help_msg = ', '.join(map(str, archs))
-    help_msg = "--> Accepted architectures: " + help_msg
-    required.add_argument("arch", nargs=1, default="",
-                          type=str, help=help_msg)
+    help_msg = "Accepted architectures: " + help_msg
+    argparser.add_argument("arch", nargs=1, default="",
+                           type=str, help=help_msg)
 
-    optional.add_argument("-d", action="store_true",
-                          help="--> Delete downloaded file")
+    argparser.add_argument("-d", action="store_false",
+                           help="Do NOT delete downloaded file")
 
-    args = argparser.parse_args()
-    return args
+    arguments = argparser.parse_args()
+    return arguments
 
 
 def clean_package_str(line: str) -> Union[List[str], List[List[str]]]:
@@ -86,7 +87,7 @@ def clean_package_str(line: str) -> Union[List[str], List[List[str]]]:
     return result
 
 
-def initiate_execution(args: argparse.Namespace) -> None:
+def initiate_execution(arguments: argparse.Namespace) -> None:
     """ Start main execution. Read architecture and download the "Contents-$arch.gz" file.
     After finalising execution delete the downloaded file.
 
@@ -94,12 +95,12 @@ def initiate_execution(args: argparse.Namespace) -> None:
         args (argparse.Namespace): Command line arguments
 
     Raises:
-        URLError: If download could not be completed from the specified URL    
+        URLError: If download could not be completed from the specified URL
         OSError: If downloaded file could not be deleted
     """
 
     uri = "http://ftp.uk.debian.org/debian/dists/stable/main/"
-    contents = "Contents-" + args.arch[0] + ".gz"
+    contents = "Contents-" + arguments.arch[0] + ".gz"
     print("Downloading file...")
     try:
         # download file from mirror link
@@ -109,48 +110,49 @@ def initiate_execution(args: argparse.Namespace) -> None:
             # File does not exist: Download
             urllib.request.urlretrieve(uri+contents, contents)
 
-        f = gzip.open(contents, mode='rt')  # read compressed file in text mode
-        Lines = f.readlines()  # read by lines
+        # read compressed file in text mode
+        contents_file = gzip.open(contents, mode='rt')
+        contents_lines = contents_file.readlines()  # read by lines
         count = 0
-        for line in Lines:
+        for line in contents_lines:
             # get the two element list from the line
             # [file, packages]
             res = clean_package_str(line)
-            # print(res)
+            print(res)
             count += 1
-            if (count == 200):
+            if count == 200:
                 break
 
         try:
-            if (args.d):
-                # attempt to delete file if specified by user
+            if args.d:
+                # attempt to delete file if not specified by user
                 os.remove(contents)
                 print("File deleted.")
         except OSError:
             print("Could not delete file: " + contents)
-            exit(-2)
-    except URLError as e:
-        print(e.reason)
+            sys.exit(-2)
+    except URLError as caught_error:
+        print(caught_error.reason)
         print("Check internet connection or specify the correct url.")
-        exit(-1)
+        sys.exit(-1)
 
 
 if __name__ == "__main__":
     accepted_architectures = ["i386", "amd64", "armel", "arm64",
                               "armhf", "mips", "mipsel", "mips64el", "ppc64el", "s390x"]
-    filename: str = __file__.split('/')[-1]
+    filename: str = __file__.rsplit('/', maxsplit=1)[-1]
     args = parse_arguments(filename, accepted_architectures)
 
     try:
         # assert (args.arch) # this scenario is handled by argparser
         # validate architecture
-        assert (args.arch[0] in accepted_architectures)
+        assert args.arch[0] in accepted_architectures
         print(*args.arch)
         initiate_execution(args)
     except AssertionError:
         # print explanatory message
         print(
-            "Error! Specify the correct required command line argument for the target architecture.\n")
+            "Error! Specify the correct target architecture.\n")
         print("Accepted architectures:\n\t", end='')
         print(*accepted_architectures, sep=', ', end='\n\n')
         print("For help, run:")
